064bc0f456645a8793125b96df5c576e2eb3fc4c
Dmitry <dima0294@yandex.ru>
2017-02-06 20:08:40 +0300

    checked mistakes

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -8,8 +8,18 @@ import java.nio.file.Paths;
 public class Producer {
 
     public static void main(String[] args) throws IOException {
-        RabbitQueue rabbitQueue = new RabbitQueue("192.168.100.124", "5672", Paths.get("/home/dimas/testec"));
-        rabbitQueue.runFilemonitorService();
+        if(args.length < 4){
+            System.out.println("Not Enough Arguments Passed");
+            System.exit(1);
+        }else {
+            String rabbitHost = args[0];
+            String rabbitPort = args[1];
+            String pathToScan = args[2];
+            String pathToAvro = args[3];
+            String fs = args[4];
+            RabbitQueue rabbitQueue = new RabbitQueue(rabbitHost, rabbitPort, Paths.get(pathToScan), pathToAvro, fs);
+            rabbitQueue.runFilemonitorService();
+        }
     }
 
 }
--- src/main/java/RabbitQueue.java
+++ src/main/java/RabbitQueue.java
@@ -1,11 +1,25 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.log4j.net.SyslogAppender;
 import org.json.simple.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.channels.FileLock;
 import java.nio.file.*;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,9 +35,11 @@ public class RabbitQueue {
     private WatchService watchService = null;
     static Logger log = Logger.getLogger(RabbitQueue.class.getName());
     private ConnectionFactory connectionFactory = new ConnectionFactory();
+    private String fs;
+    private Schema avroSchema;
     private Path pathToDirectory;
 
-    RabbitQueue(String host, String port, Path pathToDirectory){
+    RabbitQueue(String host, String port, Path pathToDirectory, String pathToAvro, String fileSystem){
         try {
             this.connectionFactory.setHost(host);
             this.connectionFactory.setPort(Integer.valueOf(port));
@@ -31,10 +47,42 @@ public class RabbitQueue {
             this.connectionFactory.setPassword("guest");
             this.pathToDirectory = pathToDirectory;
             this.watchService = this.pathToDirectory.getFileSystem().newWatchService();
-            pathToDirectory.register(watchService, ENTRY_CREATE);
+            this.fs = fileSystem;
+            this.avroSchema = new Schema.Parser().parse(new File(pathToAvro));
+            pathToDirectory.register(watchService, ENTRY_MODIFY);
             log.log(Level.INFO, "Watch logger registered for: " + pathToDirectory.getFileName());
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public List<GenericRecord> demarshallFileAvro(File file) throws IOException {
+        DatumReader<GenericRecord> reader = new GenericDatumReader<GenericRecord>(this.avroSchema);
+        DataFileReader<GenericRecord> fileReader = new DataFileReader<GenericRecord>(file, reader);
+        GenericRecord record = null;
+        List<GenericRecord> lst = new ArrayList<GenericRecord>();
+        while(fileReader.hasNext()){
+            record = fileReader.next();
+            lst.add(record);
+        }
+        fileReader.close();
+        return lst;
+    }
+
+    public byte[] getByteFromGenericRecord(List<GenericRecord> lst) throws IOException {
+        GenericDatumWriter<GenericRecord> writer = new GenericDatumWriter<GenericRecord>(this.avroSchema);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        try{
+            Encoder e = EncoderFactory.get().binaryEncoder(os, null);
+            for(GenericRecord record: lst){
+                writer.write(record, e);
+            }
+            e.flush();
+            return os.toByteArray();
+        }catch (Exception ex){
+            return null;
+        }finally {
+            os.close();
         }
     }
 
@@ -44,28 +92,18 @@ public class RabbitQueue {
             Connection connection = this.connectionFactory.newConnection();
             Channel channel = connection.createChannel();
             channel.queueDeclare(this.queueFileMonitorService, true, false, false, null);
-            for(;;){
-                WatchKey key = this.watchService.take();
-                for(WatchEvent<?> event: key.pollEvents()){
-                    WatchEvent.Kind<?> kind = event.kind();
-                    @SuppressWarnings("unchecked")
-                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
-                    Path fileName = ev.context();
-                    log.log(Level.INFO, kind.name() + " has occurred with file : " + fileName.toString());
-                    boolean valid = key.reset();
-                    if (!valid) {
-                        break;
-                    }
-                    String pathAbsolute = this.pathToDirectory.toString() + "/" + fileName.toString();
-                    jsonObject.put("event", kind.name());
-                    jsonObject.put("path", pathAbsolute);
-                    channel.basicPublish("", this.queueFileMonitorService, null, jsonObject.toJSONString().getBytes());
-                }
-
+            File folder = new File(this.pathToDirectory.toString());
+            for(File file: folder.listFiles()){
+                log.log(Level.INFO, "Processing path... ");
+                jsonObject.put("path", file.getPath());
+                jsonObject.put("data", new Timestamp(System.currentTimeMillis()).toString());
+                channel.basicPublish("", this.queueFileMonitorService, null, jsonObject.toString().getBytes());
+                log.log(Level.INFO, "Processed path! And sent to Rabbit !!!");
             }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+        }catch (Exception ex){
+
         }
+
     }
 
 }
